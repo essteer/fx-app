3dc434c6ed45210ce61c92326a8aecb06adda790
elliott.steer <elliott.steer@fdmgroup.com>
2024-07-26 15:09:23 +0800

    feat: Add Currency class and unit tests

---
+++ src/main/java/com/fdmgroup/fx_app/Currency.java
@@ -0,0 +1,41 @@
+package com.fdmgroup.fx_app;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Currency {
+	
+	private String code;
+	private double rate;
+	private double inverseRate;
+	
+	@JsonCreator
+	public Currency(
+			@JsonProperty("code") String code, 
+			@JsonProperty("rate") double rate,
+			@JsonProperty("inverseRate") double inverseRate) {
+		
+		this.code = code;
+		this.rate = rate;
+		this.inverseRate = inverseRate;
+	}
+	
+	public String getCode() {
+		return this.code;
+	}
+	
+	public double getRate() {
+		return this.rate;
+	}
+	
+	public double getInverseRate() {
+		return this.inverseRate;
+	}
+	
+	@Override
+	public String toString() {
+		return this.code + " " + this.rate + " " + this.inverseRate;
+	}
+}
---
+++ src/test/java/com/fdmgroup/fx_app/CurrencyTest.java
@@ -0,0 +1,22 @@
+package com.fdmgroup.fx_app;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+public class CurrencyTest {
+	
+	@Test
+	@DisplayName("Currency objects initialise correctly")
+	public void test_Currency_created() {
+		String code = "usd";
+		double rate = 1.0;
+		double inverseRate = 1.0;
+		Currency usd = new Currency(code, rate, inverseRate);
+		
+		assertEquals(code, usd.getCode());
+		assertEquals(rate, usd.getRate());
+		assertEquals(inverseRate, usd.getInverseRate());
+		assertEquals(code + " " + rate + " " + inverseRate, usd.toString());
+	}
+
+}
